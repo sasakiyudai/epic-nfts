d22069e161ed75686b92c4db6c3a72dde9331a6f
sasakiyudai <yudai14142@gmail.com>
2022-02-03 01:20:01 +0900

    🤠 Dynamically generating SVG NFTs on-chain

--- scripts/run.js
+++ scripts/run.js
@@ -3,7 +3,7 @@ const main = async () => {
 	const nftContract = await nftContractFactory.deploy();
 	await nftContract.deployed();
 	console.log("Contract deployed to:", nftContract.address);
-	console.log(process.env)
+
 	let txn = await nftContract.makeAnEpicNFT()
 	await txn.wait()
 
